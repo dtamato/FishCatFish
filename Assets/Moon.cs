0b9d7a447f19a553bd99496d664764a5b0328ce9
Dave Tamayo <djbtamayo@gmail.com>
2016-10-28 15:42:12 -0400

    Initial push of Unity project. Started working on the Moon behavior.

---
+++ Assets/Moon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class Moon : MonoBehaviour {
+
+	[SerializeField] int minMoonSize;
+	[SerializeField] int maxMoonSize;
+
+	[SerializeField] float minIdleSeconds;
+	[SerializeField] float maxIdleSeconds;
+
+	[SerializeField] float sizeChangeSpeed;
+
+	[SerializeField] SpriteRenderer waterSpriteRenderer;
+
+	bool changingSize = false;
+	float currentSize;
+	float newSize;
+
+	// Use this for initialization
+	void Start () {
+	
+		currentSize = this.transform.localScale.x;
+		StartCoroutine (SetNewSize ());
+	}
+
+	void Update () {
+
+		if (changingSize) {
+
+			if (Mathf.Abs(currentSize - newSize) <= 0.01f) {
+
+				if(newSize == 1) { currentSize = 1; }
+				else if(newSize == 3) { currentSize = 3; }
+				this.transform.localScale = new Vector3 (currentSize, currentSize, 1);
+
+				changingSize = false;
+				StartCoroutine (SetNewSize ());
+			}
+			else if (currentSize != newSize) {
+
+				currentSize = Mathf.Lerp (currentSize, newSize, sizeChangeSpeed * Time.deltaTime);
+				this.transform.localScale = new Vector3 (currentSize, currentSize, 1);
+			}
+		}
+	}
+
+	public IEnumerator SetNewSize () {
+
+		yield return new WaitForSeconds (Random.Range (minIdleSeconds, maxIdleSeconds));
+
+		newSize = (currentSize == 1) ? 3 : 1;
+		changingSize = true;
+	}
+}
