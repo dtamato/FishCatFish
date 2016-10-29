7c3225002398441d6c8a65962c9b1f993a943f9f
Dave Tamayo <djbtamayo@gmail.com>
2016-10-29 18:52:39 -0400

    Started working on the Lobby scene

---
+++ Assets/Scripts/PlayerSelectController.cs
@@ -0,0 +1,29 @@
+﻿using Rewired;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class PlayerSelectController : MonoBehaviour {
+
+	[SerializeField] int playerID;
+	[SerializeField] GameObject playerEnterObject;
+	[SerializeField] GameObject characterSelect;
+
+	Player player;
+
+	void Awake() {
+
+		player = ReInput.players.GetPlayer (playerID);
+		characterSelect.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (player.GetButtonDown ("UISubmit")) {
+
+			playerEnterObject.SetActive (false);
+			characterSelect.SetActive (true);
+		}
+	}
+}
